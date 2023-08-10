b0fbdf57cbe14e30bd9d0c12241b4c74467475f1
duybin45 <92349525+duybin45@users.noreply.github.com>
2023-08-10 10:02:47 +0700

    update produce

---
+++ src/pages/Home/Produce/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Col, Image, Row } from "antd";
+import style from "./style.module.css";
+
+const Produce = () => {
+  const datafake = [
+    {
+      img: "sss",
+      name: "Màn hình AOC 27G2SP 27 IPS 165Hz Gsync compatible chuyên gaming",
+      oldPrice: "5.190.000₫",
+      newPrice: "4.690.000₫",
+      sale: "-34%",
+    },
+    {
+      img: "sss1",
+      name: "Màn hình AOC 27G2SP 27 IPS 165Hz Gsync compatible chuyên gaming",
+      oldPrice: "5.190.000₫",
+      newPrice: "4.690.000₫",
+      sale: "-34%",
+    },
+  ];
+  return (
+    <div className={style.wrap_produce}>
+      {datafake?.map((e) => (
+        <div className={style.items_produce}>
+          <div className={style.produce_img}>
+            <Image src={e?.img} alt="" />
+          </div>
+          <div className={style.produce_content}>
+            <h1 className={style.produce_title}>{e?.name}</h1>
+            <span className={style.produce_price}>{e?.oldPrice}</span>
+            <div className={style.wrap_price_new}>
+              <span className={style.newPrice}>{e?.newPrice}</span>
+              <span className={style.sale}>{e?.sale}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Produce;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { APIService } from "../../services/apiService";
 
-import Produce from "./Produce/produce";
+import Produce from "./Produce";
 
 const HomePage = () => {
-
-  const [listData, setListData] = useState()
+  const [listData, setListData] = useState();
 
   async function fetchData() {
     try {
       const data = await APIService.get_ListData();
       console.log(data);
-      setListData(data)
+      setListData(data);
     } catch (error) {}
   }
   useEffect(() => {
