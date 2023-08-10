e156dcab09949d228caa55d6022701f67daf7d2c
duybin45 <92349525+duybin45@users.noreply.github.com>
2023-08-10 08:33:19 +0700

    update

---
+++ src/pages/Home/Produce/produce.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Col, Row } from "antd";
+
+const produce = () => {
+  return (
+    <div>
+      <Row gutter={[16, 8]}>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+      </Row>
+      <Row gutter={[16, 8]}>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+        <Col span={6}>sss</Col>
+      </Row>
+    </div>
+  );
+};
+
+export default produce;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { APIService } from "../../services/apiService";
 
+import Produce from "./Produce/produce";
+
 const HomePage = () => {
+
+  const [listData, setListData] = useState()
+
   async function fetchData() {
     try {
       const data = await APIService.get_ListData();
       console.log(data);
+      setListData(data)
     } catch (error) {}
   }
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <div>xin chao</div>;
+  return (
+    <div>
+      <Produce />
+    </div>
+  );
 };
 
 export default HomePage;
