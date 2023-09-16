50e0ef3a7bbf67b520fc0420a875a3e07b9735bd
Ho Phuong <hophuong.htp.99@gmail.com>
2023-09-16 10:19:32 +0700

    up

--- src/components/ProductPayment/index.jsx
+++ src/components/ProductPayment/index.jsx
@@ -1,15 +1,53 @@
 import React, { useEffect, useState } from "react";
 import style from "./style.module.css";
-import { Alert, Button, Col, Row, Select } from "antd";
+import "./custome.css";
+import { Alert, Button, Col, Form, Input, Row, Select, Typography } from "antd";
 import { DatePicker, Space } from "antd";
 import moment from "moment";
 import { LoadingOutlined } from "@ant-design/icons";
+import { useForm } from "antd/es/form/Form";
+import { Option } from "antd/es/mentions";
 
 const ProductPayment = () => {
+  const listOrder = [
+    {
+      id: 1,
+      name: "laptop Asus",
+      price: 1000,
+      number: "1",
+      totalPrice: 2000,
+    },
+    {
+      id: 2,
+
+      name: "laptop Lenovo",
+      price: 2000,
+      number: "2",
+      totalPrice: 4000,
+    },
+  ];
+
+  const [form] = Form.useForm();
+
   const [date, setDate] = useState(null);
   const [dateShip, setDateShip] = useState(null);
   const [payment, setPayment] = useState(null);
   const [loading, setLoading] = useState(false);
+  const config = {
+    rules: [
+      {
+        type: "object",
+        required: true,
+        message: "Please select time!",
+      },
+    ],
+  };
+
+  let allPriceItem = listOrder.map((items) => items.totalPrice);
+
+  const totalPriceOder = allPriceItem.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
 
   const currentDay = moment();
   const PickDate = (date, dateString) => {
@@ -22,132 +60,150 @@ const ProductPayment = () => {
     setPayment(value);
   };
 
-  console.log(payment);
+  const submitFormOrder = () => {
+    const note = form.getFieldValue("note");
+    const datePicker = form.getFieldValue("select-date");
+    const expectedDate = form.getFieldValue("expected-date", dateShip);
+    const optionPay = form.getFieldValue("optionPay");
+    const totalOrder = form.getFieldValue("totalPriceOder", totalPriceOder);
+
+    const data = {
+      note: note,
+      datePicker: datePicker,
+      expectedDate: expectedDate,
+      optionPay: optionPay,
+      totalOrder: totalOrder,
+    };
+
+    console.log(data);
+  };
 
   return (
     <>
       <div className={style.container}>
-        <div className={style.infor_Product}>
-          <Row gutter={[20, 20]}>
-            <Col span={24}>
-              <Row>
-                <Col span={12}>
-                  <div className={style.content_Produt}>
-                    <p>Sản phẩm</p>
-                  </div>
-                </Col>
-                <Col span={4}>
-                  <div className={style.content_Produt}>
-                    <p>Đơn giá</p>
-                  </div>
-                </Col>
-                <Col span={4}>
-                  <div className={style.content_Produt}>
-                    <p>Số lượng</p>
-                  </div>
-                </Col>
-                <Col span={4}>
-                  <div className={style.content_Produt}>
-                    <p>Thành tiền</p>
-                  </div>
-                </Col>
-              </Row>
-            </Col>
-            <Col span={24}>
-              <Row>
-                <Col span={12}>
-                  <div className={style.content_Produt}>
-                    <p>Laptop Gaming</p>
-                  </div>
-                </Col>
-                <Col span={4}>
-                  <div className={style.content_Produt}>
-                    <p>₫ 149.310 </p>
-                  </div>
-                </Col>
-                <Col span={4}>
-                  <div className={style.content_Produt}>
-                    <p>1</p>
-                  </div>
-                </Col>
-                <Col span={4}>
-                  <div className={style.content_Produt}>
-                    <p>₫149.310</p>
-                  </div>
-                </Col>
-              </Row>
-            </Col>
-            <Col span={24}>
-              <Row gutter={[20, 20]}>
-                <Col span={10}>
-                  <div className={style.noteMessage}>
-                    <span>Lời nhắn :</span>
-                    <input placeholder="Lưu ý cho shop" />
-                  </div>
-                </Col>
-                <Col span={6}>
-                  <Row gutter={[10, 10]}>
+        <div className="formOrder">
+          <Form
+            form={form}
+            className="formOrder"
+            size="middle"
+            labelCol={{ span: 12 }}
+          >
+            <Row gutter={[20, 20]}>
+              <Col span={24}>
+                <Row>
+                  <Col span={12}>
+                    <div className={style.content_Produt}>
+                      <p>Sản phẩm</p>
+                    </div>
+                  </Col>
+                  <Col span={4}>
+                    <div className={style.content_Produt}>
+                      <p>Đơn giá</p>
+                    </div>
+                  </Col>
+                  <Col span={4}>
+                    <div className={style.content_Produt}>
+                      <p>Số lượng</p>
+                    </div>
+                  </Col>
+                  <Col span={4}>
+                    <div className={style.content_Produt}>
+                      <p>Thành tiền</p>
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+
+              <Col span={24}>
+                {listOrder.map((item) => (
+                  <Row key={item.id}>
                     <Col span={12}>
-                      <Row gutter={[20, 20]}>
-                        <span>Thời gian vận chuyển : </span>
-                        <span>Thời gian dự kiến : </span>
-                      </Row>
+                      <div className={style.content_Produt}>
+                        <p>{item.name}</p>
+                      </div>
                     </Col>
-                    <Col span={12}>
-                      <Row gutter={[5, 5]}>
-                        <Space direction="vertical">
+                    <Col span={4}>
+                      <div className={style.content_Produt}>
+                        <p>{item.price}</p>
+                      </div>
+                    </Col>
+                    <Col span={4}>
+                      <div className={style.content_Produt}>
+                        <p>{item.number}</p>
+                      </div>
+                    </Col>
+                    <Col span={4}>
+                      <div className={style.content_Produt}>
+                        <p>{item.totalPrice}</p>
+                      </div>
+                    </Col>
+                  </Row>
+                ))}
+              </Col>
+              <Col span={24}>
+                <Row gutter={[20, 20]}>
+                  <Col span={8}>
+                    <Form.Item name="note" label="Lưu ý cho shop">
+                      <Input.TextArea showCount maxLength={100} />
+                    </Form.Item>
+                  </Col>
+                  <Col span={8}>
+                    <Row gutter={[10, 10]}>
+                      <Col span={24}>
+                        <Form.Item
+                          name="select-date"
+                          label="Chọn thời gian vận chuyển : "
+                          {...config}
+                        >
                           <DatePicker
                             onChange={PickDate}
                             format={"DD-MM-YYYY"}
                             locale
                             showToday={true}
-                            defaultValue={moment()}
+                            // defaultValue={moment()}
                           />
-                        </Space>
-                        <span>{dateShip} </span>
-                      </Row>
-                    </Col>
-                  </Row>
-                </Col>
+                        </Form.Item>
+                        <Form.Item name="expected-date" label="Ngày dự kiến ">
+                          <Typography>{dateShip}</Typography>
+                        </Form.Item>
+                        <Form.Item
+                          name="optionPay"
+                          label="Chọn phương thức thanh toán :"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Please select gender!",
+                            },
+                          ]}
+                        >
+                          <Select placeholder="Chọn phương thức !">
+                            <Option value="cash">Tiền Mặt</Option>
+                            <Option value="bank">Chuyển Khoản</Option>
+                          </Select>
+                        </Form.Item>
+                      </Col>
+                    </Row>
+                  </Col>
 
-                <Col span={8}>
-                  <Row gutter={[20, 20]}>
-                    <Col>
-                      <span> Chọn phương thức thanh toán : </span>
-                    </Col>
-                    <Col>
-                      <Select
-                        showSearch
-                        placeholder="Select a person"
-                        optionFilterProp="children"
-                        onChange={onChange}
-                        options={[
-                          {
-                            value: "cash",
-                            label: "Tiền Mặt",
-                          },
-                          {
-                            value: "bank",
-                            label: "Chuyển Khoản",
-                          },
-                        ]}
-                      />
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row justify={"end"}>
-            <Col span={4}>
-              <p>Tổng Tiền</p>
-            </Col>
-          </Row>
-          <Row justify={"center"}>
-            <Button>
-              Default Button <LoadingOutlined />
-            </Button>
-          </Row>
+                  <Col span={8}>
+                    <Row justify={"center"}>
+                      <Form.Item name="totalOrder" label="Tổng Tiền :">
+                        <p>{totalPriceOder}</p>
+                      </Form.Item>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+
+            <Row justify={"center"}>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Register
+                </Button>
+              </Form.Item>
+            </Row>
+          </Form>
         </div>
       </div>
     </>
